dc02c9e563bbad36a3767a79af5681f3cf7849b1
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2020-10-14 11:14:21 -0600

    usersgroups page is now working, can visit page, not attached to base

--- usersgroups/views.py
+++ usersgroups/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Usersgroups
+
+class GroupListView(ListView):
+    model = Usersgroups
+    template_name = 'group_list.html'
 
 # Create your views here.
