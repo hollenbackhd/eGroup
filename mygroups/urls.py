db9fa735df90038e94ebf271369d73e6b4063c77
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2020-10-30 19:08:42 -0600

    updated urls

--- mygroups/urls.py
+++ mygroups/urls.py
@@ -1,10 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from .views import MygroupsDetailView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
-    path('allgroups/', include('allgroups.urls')),
-    path('', include('pages.urls')),
+    path('<int:pk>/', MygroupsDetailView.as_view(), name='mygroups_detail')
     ]
