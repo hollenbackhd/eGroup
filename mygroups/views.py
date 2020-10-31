3e0a149c98a1ee9917919825a17de2c2a969289d
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2020-10-30 18:44:41 -0600

    adding loginRequired and Userpasses to mygroups views, will need to be signed in to access page

--- mygroups/views.py
+++ mygroups/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin
+    )
 
 def index(request):
     return HttpResponse("My Groups")
