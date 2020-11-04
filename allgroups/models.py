b0c05b75c8560b1cd15d23dbfdc477e86968e0c5
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2020-11-04 16:35:05 -0700

    updated allgroups models to include request - set at false

--- allgroups/models.py
+++ allgroups/models.py
@@ -7,6 +7,7 @@ class Allgroups(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    request = models.BooleanField(default=False);
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
